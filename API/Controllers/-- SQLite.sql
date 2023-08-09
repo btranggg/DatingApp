b27aa2857414d7b9ef7d0f8d3dbd09b941ca7055
Brian Trang <btrang@assessor.lacounty.gov>
2023-08-09 10:23:19 -0700

    end of section 2

---
+++ API/Controllers/-- SQLite.sql
@@ -0,0 +1,11 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1, "Blane");
+
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (2, "Erika");
+
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (3, "Giovanni");
